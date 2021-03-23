5b1b346da3ff6951042dc296a756c774ae065487
Mis-programmation <julebreezy4@gmail.com>
2021-03-23 22:40:16 +0000

    ajout de webpack et bootsrap & et la page detail profil

---
+++ src/Controller/HomePage.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @author       : Soko Mamadou ibrahima
+ * @email        : mamadousoko98@gmail.com
+ * @phone_number : +221772199737 / +22248796401
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HomePage
+ * @package App\Controller
+ */
+class HomePage extends AbstractController
+{
+    /**
+     * @Route("/",name="index")
+     * @return Response
+     */
+    public function index():Response
+    {
+        return $this->render("homePage/index.html.twig");
+    }
+
+    /**
+     * @Route("/profil/detail",name="profil_detail_cv")
+     * @return Response
+     */
+    public function detailCv():Response
+    {
+        return $this->render("profile/cv/detailCv.html.twig");
+    }
+}
